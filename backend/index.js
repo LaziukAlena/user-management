40f07f1b617d4c90a7da809635eb663a41f18107
LaziukAlena <Lazyuk.elena1988@gmail.com>
2025-08-01 16:59:16 +0300

    Fix: use process.env.PORT for deployment

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001; 
 
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
