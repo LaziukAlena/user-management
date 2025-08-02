902abcc8e4abf6e0ba3d8dc2293ff38eb584628b
LaziukAlena <Lazyuk.elena1988@gmail.com>
2025-08-02 11:59:58 +0300

    Update index.js
    изменили порьт

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3001; 
+const PORT = process.env.PORT || 8080;
+ 
 
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
